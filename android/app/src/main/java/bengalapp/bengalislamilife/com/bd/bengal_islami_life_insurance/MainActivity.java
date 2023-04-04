0e0b9b7541749edaf96a5d8f5cb19d63e467c697
shakawat <md.shakawat.hossain@outlook.com>
2023-04-04 20:40:50 +0600

    Bengal Islami Life Insurance multiplatform applications

---
+++ android/app/src/main/java/bengalapp/bengalislamilife/com/bd/bengal_islami_life_insurance/MainActivity.java
@@ -0,0 +1,6 @@
+package bengalapp.bengalislamilife.com.bd.bengal_islami_life_insurance;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
