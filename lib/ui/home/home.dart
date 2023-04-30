1808e34895886879db713b73568b38e5c2700665
shakawat <md.shakawat.hossain@outlook.com>
2023-04-30 18:53:06 +0600

    home fix

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeSection extends StatefulWidget {
@@ -14,7 +15,23 @@ class _HomeSectionState extends State<HomeSection> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-
+            CarouselSlider(
+              options: CarouselOptions(height: 200.0),
+              items: [1,2,3,4,5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                            color: Colors.amber
+                        ),
+                        child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                    );
+                  },
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
